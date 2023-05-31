5180c24b32f12edd95390c3fffe7a0e519d5408c
Mateus Elias Vieira <mateusifg16@gmail.com>
2023-05-30 21:43:06 -0300

    atualizado

--- src/main/java/br/com/aprendendo/controller/PessoaController.java
+++ src/main/java/br/com/aprendendo/controller/PessoaController.java
@@ -1,6 +1,11 @@
 package br.com.aprendendo.controller;
 
+import java.util.List;
+
+import javax.xml.transform.Source;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.PayloadEndpoint;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
@@ -17,10 +22,18 @@ public class PessoaController {
 	@Autowired
 	private PessoaService pessoaService;
 	
-	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "mensagemPessoaEntrada")
+	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "InserirPessoaRequest")
 	@ResponsePayload
 	public PessoaModel inserirPessoaModel(@RequestPayload PessoaModel pessoaModel) {
 		return pessoaService.inserirPessoaModel(pessoaModel);
 	}
+	
+	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "ListarPessoaRequest")
+	@ResponsePayload
+	public List<PessoaModel> listarPessoaModel(@RequestPayload String filter) {
+		System.out.println("Entrou aqui");
+		return pessoaService.listarTodasPessoasModel();
+	}
+
 	
 }
---
+++ src/main/java/br/com/aprendendo/filter/CorsFilter.java
@@ -0,0 +1,30 @@
+package br.com.aprendendo.filter;
+
+import java.io.IOException;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        chain.doFilter(req, res);
+    }
+}
